122ae6a5d019b913218b579c4cf6defbe753d359
MaLiVer <maliverko@gmail.com>
2024-01-24 12:38:49 +0500

    Добавьте файлы проекта.

---
+++ Lesson1/Lesson1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+void copyArray(int* source, int* destination, int size) {
+    for (int i = 0; i < size; i++) {
+        *(destination + i) = *(source + i);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    const int size = 5;
+
+    int sourceArray[size] = { 1, 2, 3, 4, 5 };
+    int destinationArray[size];
+
+    copyArray(sourceArray, destinationArray, size);
+
+    std::cout << "Исходный массив: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << sourceArray[i] << " ";
+    }
+
+    std::cout << "\nСкопированный массив: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << destinationArray[i] << " ";
+    }
+
+    return 0;
+}
